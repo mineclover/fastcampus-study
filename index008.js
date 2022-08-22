c0d6b72314ca89144f24a432e9903679b65afa10
mineclover <mineclover@naver.com>
2022-08-23 02:01:12 +0900

    js를 빠르게 조저보았다 으악

--- index008.js
+++ index008.js
@@ -28,7 +28,7 @@ function startTextChange() {
 
 function intervalSalutations() {
   
-  var salutations = ["안녕하세요", "반갑습니다"];  
+  let salutations = ["안녕하세요", "반갑습니다"];  
   // 아 0일 땐 1-0되서 1 , 1일 땐 .. 뭐 그런건데 리액트나 js 로 옮긴 상태에서도 가능한가?
   console.log(i)
   console.log("inter"+stopIntervalSalutations);
@@ -36,3 +36,39 @@ function intervalSalutations() {
   document.querySelector(".chapter02").innerHTML = salutations[i];
 }
 
+function showMyAlert() {
+  alert("짠! 알림창이 나타났습니다.");
+}
+
+function numberTypeCheck() {
+  document.querySelector("#typesTest").innerHTML = `100의 typeof : ${typeof(100)}`;
+}
+function stringTypeCheck() {
+  document.querySelector("#typesTest").innerHTML = `"100"의 typeof : ${typeof("100")}`;
+}
+function functionTypeCheck() {
+  let a = function(){};
+  document.querySelector("#typesTest").innerHTML = `a(함수를 넣은)의 typeof : ${typeof(a)}`;
+}
+function nullTypeCheck() {
+  let a = null;
+  document.querySelector("#typesTest").innerHTML = `a(null 넣은)의 typeof : ${typeof(a)}`;
+}
+
+// 자료형 연산
+function numToNum() {
+  let a = 10;
+  document.querySelector("#typescalc").innerHTML = `10 + a : ${10 + a}`;
+}
+
+
+function stringToNum() {
+  let a = 10;
+  document.querySelector("#typescalc").innerHTML = `"10" + a : ${"10" + a}`;
+}
+
+function three(number){
+  var largerNumber = number > 10 ? "참" : "거짓";
+  document.querySelector("#threeresult").innerHTML = largerNumber;
+  console.log(largerNumber);
+}
