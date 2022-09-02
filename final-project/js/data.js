a7504072da1521a3a79c79ff630d2101be3577e1
mineclover <mineclover@naver.com>
2022-09-03 00:36:29 +0900

    반 정도 자동화 됨

--- final-project/js/data.js
+++ final-project/js/data.js
@@ -1,24 +1,236 @@
-var game_01 = {
+var game = [];
+var edu = [];
+var music = [];
+var code = [];
+var pit = [];
+var dumi = [];
+
+
+game.push({
   thumbnail : "dumidata/green.jpg",
-  logo : 'dumidata/logo3.jpg',
-  title: '초록색 인형이 나오는 게임',
-  username : '테스트',
+  logo : 'dumidata/logo_03.jpg',
+  title: '링크 있는 데이터 green',
+  username : '나머지는 링크 # 처리',
+  contentLink : 'https://www.youtube.com/watch?v=Aj_lUHETGl0',
   channelLink : 'https://www.youtube.com/channel/UCiv2gnRHHWjKAOLVkOUGCEw',
   official : true,
-  view : 50000,
-  created : 2022-09-02,
-  live : true
-}
+  view : '조회수 1.4만회·',
+  created : '3일전',
+  live : false
+});
 
 
-var game_02 = {
-  thumbnail : '이미지',
-  logo : '이미지',
-  title: '영상제목',
-  username : '작성자',
-  channelLink : '홈페이지 경로',
+edu.push ({
+  thumbnail : 'dumidata/thummbnail_06.jpg',
+  logo : 'dumidata/logo_17.jpg',
+  title: '스티부가 알려주는 진짜 쉬운 프로그래밍',
+  username : '스티부',
+  contentLink : '#',
+  channelLink : '#',
   official : true,
-  view : 50000,
-  created : 2022-09-02,
+  view : '5.3명 시청중',
+  created : '',
   live : true
+});
+
+music.push( {
+  thumbnail : 'dumidata/thummbnail_15.jpg',
+  logo : 'dumidata/logo_24.jpg',
+  title: '피곤할 때 듣는 노래 24시간',
+  username : '수면테라피',
+  contentLink : '#',
+  channelLink : '#',
+  official : false,
+  view : '조회수 164만회·',
+  created : '1달전',
+  live : false
+} );
+
+music.push ( {
+  thumbnail : 'dumidata/thummbnail_21.jpg',
+  logo : 'dumidata/logo_28.jpg',
+  title: '아이돌 공연 댓글 모음 ver.28',
+  username : '사생팬 아닙니다',
+  contentLink : '#',
+  channelLink : '#',
+  official : false,
+  view : '조회수 1064만회·',
+  created : '3달전',
+  live : false
+});
+
+music.push ( {
+  thumbnail : 'dumidata/thummbnail_13.jpg',
+  logo : 'dumidata/logo_30.jpg',
+  title: '집중 잘되는 음악 실시간 Live',
+  username : '잔잔한 음악',
+  contentLink : '#',
+  channelLink : '#',
+  official : false,
+  view : '913명 시청중',
+  created : '',
+  live : true
+});
+
+music.push ( {
+  thumbnail : 'dumidata/thummbnail_27.jpg',
+  logo : 'dumidata/logo_30.jpg',
+  title: '편안한 분위기 플레이리스트',
+  username : '잔잔한 음악',
+  contentLink : '#',
+  channelLink : '#',
+  official : false,
+  view : '조회수 4.6만회',
+  created : '',
+  live : true
+});
+
+pit.push ( {
+  thumbnail : 'dumidata/thummbnail_29.jpg',
+  logo : 'dumidata/logo_10.jpg',
+  title: '100일 완성 건강 루틴 피트니스 3일차',
+  username : '신나는 요가',
+  contentLink : '#',
+  channelLink : '#',
+  official : true,
+  view : '93명 시청중',
+  created : '',
+  live : true
+});
+
+pit.push({
+  thumbnail : 'dumidata/thummbnail_29.jpg',
+  logo : 'dumidata/logo_10.jpg',
+  title: '100일 완성 건강 루틴 피트니스 2일차',
+  username : '신나는 요가',
+  contentLink : '#',
+  channelLink : '#',
+  official : true,
+  view : '조회수 3001회·',
+  created : '스트리밍 시간 : 1일전',
+  live : false
+});
+
+
+code.push( {
+  thumbnail : 'dumidata/thummbnail_28.jpg',
+  logo : 'dumidata/logo_25.jpg',
+  title: 'IT 인재가 되기 위한 101가지',
+  username : '국제IT인력양성협회',
+  contentLink : '#',
+  channelLink : '#',
+  official : true,
+  view : '조회수 3001회·',
+  created : '2주전',
+  live : false
+});
+
+code.push( {
+  thumbnail : 'dumidata/thummbnail_22.jpg',
+  logo : 'dumidata/logo_16.jpg',
+  title: '야너두 할 수 있다! 파이썬 업무 자동화',
+  username : '너두코딩',
+  contentLink : '#',
+  channelLink : '#',
+  official : false,
+  view : '조회수 3.3만회·',
+  created : '1년전',
+  live : false
+});
+
+code.push({
+  thumbnail : 'dumidata/thummbnail_22.jpg',
+  logo : 'dumidata/logo_16.jpg',
+  title: '야너두 할 수 있다! 파이썬 업무 자동화-02',
+  username : '너두코딩',
+  contentLink : '#',
+  channelLink : '#',
+  official : false,
+  view : '조회수 3.3만회·',
+  created : '1년전',
+  live : false
+});
+
+code.push({
+  thumbnail : 'dumidata/thummbnail_14.jpg',
+  logo : 'dumidata/logo_16.jpg',
+  title: '고마워 과거의 나! 특집 파이썬 특강',
+  username : '너두코딩',
+  contentLink : '#',
+  channelLink : '#',
+  official : false,
+  view : '조회수 3천회·',
+  created : '5일전',
+  live : false
+});
+
+code.push({
+  thumbnail : 'dumidata/thummbnail_14.jpg',
+  logo : 'dumidata/logo_16.jpg',
+  title: '신입이니? 이거 공부해와! 파이썬 업무 자동화-01',
+  username : '너두코딩',
+  contentLink : '#',
+  channelLink : '#',
+  official : false,
+  view : '조회수 10.2만회·',
+  created : '1년전',
+  live : false
+});
+
+
+function videoRandomGen(num){
+
+  for(let i = 0; i < num; i++){
+    var randomNum = [];
+    randomNum.push(Math.floor(Math.random() * 34 + 1));
+    //썸네일 수
+    randomNum.push(Math.floor(Math.random() * 33 + 1));
+    //로고 수
+    //https://blogpack.tistory.com/600
+    //숫자 자릿수 맞추기
+    randomNum[0] =  '' + randomNum[0];
+    randomNum[0] = randomNum[0].padStart(2,'0');
+    
+    randomNum[1] =  '' + randomNum[1];
+    randomNum[1] = randomNum[1].padStart(2,'0');
+
+    randomNum.push(Boolean(Math.floor(Math.random()*2)));
+    //floor 가 잘 안되서 round 로 바꿈 다시 floor로 돌림
+    //official 랜덤
+    console.log(randomNum[2]);
+    
+      console.log(randomNum[2]);
+      dumi.push ({
+        thumbnail : `dumidata/thummbnail_${randomNum[0]}.jpg`,
+        logo : `dumidata/logo_${randomNum[1]}.jpg`,
+        title: `아무 컨텐츠_0${dumi.length}`,
+        username : `user_0${dumi.length}`,
+        contentLink : '#',
+        channelLink : '#',
+        official : randomNum[2],
+        view : '조회수 N만회·',
+        created : 'N일전',
+        live : false
+      });
+      
+    
+    // else if(name == 'music') {
+      
+    //   music.push ({
+    //     thumbnail : `dumidata/thummbnail_${randomNum[0]}.jpg`,
+    //     logo : `dumidata/logo_${randomNum[1]}.jpg`,
+    //     title: `아무 게임_0${music.length}`,
+    //     username : `musician_0${music.length}`,
+    //     contentLink : '#',
+    //     channelLink : '#',
+        
+    //     official : randomNum[2],
+    //     view : '조회수 N만회·',
+    //     created : 'N일전',
+    //     live : false
+    //   });
+    // }
+
+
+  }
 }
--- final-project/js/index.js
+++ final-project/js/index.js
@@ -17,40 +17,46 @@ function videoContent(num){
 }
 
 function contentSelector(){
-  var list = document.querySelectorAll('.primary-area > article.content');
+  var list = document.querySelectorAll('.primary-area article.content');
   console.log(list.length);
 
+  console.log(list[0]);
+  
+  
+
   for(let i = 0;i <list.length;i++ ){
-    videoGenerator(list[i],game_01);
+    videoGenerator(list[i],dumiGen(i));
   }  
 }
+function dumiGen(i){
+
+  while (i >= dumi.length){
+    videoRandomGen(1);
+  }
+  return dumi[i];
+}
+
 
 function videoGenerator(element,object) {
 
-  console.log(object.thumbnail);
-  console.log(object.logo);
-  console.log(object.title);
-  console.log(object.username);
-  console.log(object.channelLink);
-  console.log(object.official );
-  console.log(object.view);
-  console.log(object.created);
-  console.log(object.live);
+  //document.querySelector().innerHTML
+  element.querySelector('.vidio_thumb').setAttribute("href",object.contentLink);
+  element.querySelector('.content__img').setAttribute("src",object.thumbnail);
+  element.querySelector('.content__logo').setAttribute("src",object.logo);
+  element.querySelector('.content__title').setAttribute("href",object.contentLink);
+  element.querySelector('.content__title').innerHTML = object.title ;
+  element.querySelector('.content__info').innerHTML = `${object.view}${object.created}`;
   
+  element.querySelector('.content__user').setAttribute("href",object.channelLink);
+  element.querySelector('.content__live').style.display = object.live ? 'block' : 'none';
   
-  var list = document.querySelectorAll('');
-  
-  let a = list[0].querySelector('content__img');
-  a.setAttribute('src','object.thumbnail');
-  
-  
-  // object.thumbnail
-  // object.logo
-  // object.title
-  // object.username
-  // object.channelLink
-  // object.official 
-  // object.view
-  // object.created
-  // object.live
+  // console.log(object.thumbnail);
+  // console.log(object.logo);
+  // console.log(object.title);
+  // console.log(object.username);
+  // console.log(object.channelLink);
+  // console.log(object.official );
+  // console.log(object.view);
+  // console.log(object.created);
+  // console.log(object.live);
 }
